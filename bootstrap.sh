963b8b7ff3e80c44ab12b21a21811965ddd41b72
Ken Burres <ken@khbtechservices.com>
2026-02-27 21:01:04 -0500

    Initial Commit

---
+++ bootstrap.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+set -e #exit on errors
+
+# -------------------------------
+# 1. Append custom .bashrc lines
+#--------------------------------
+BASHRC="$HOME/.bashrc"
+CUSTOM_BASHRC="custom_bashrc"
+CUSTOM_MARKER="#== KHB3 LINUX BOOTSTRAP ==#"
+
+# Only append if not present
+if ! grep -q "$CUSTOM_MARKER" "$BASHRC"; then
+    echo "Writing custom .bashrc lines to ${BASHRC}..."
+    echo "" >> "$BASHRC"
+    echo "$CUSTOM_MARKER" >> "$BASHRC"
+    echo "" >> "$BASHRC"
+    echo "$CUSTOM_BASHRC" >> "$BASHRC"
+
+    # Source the file
+    source "$BASHRC"
+    echo "Sourced $BASHRC"
+fi
+
+
+# -------------------------------
+# 2. Install vim-plug
+#--------------------------------
+
+if [ ! f "$HOME/.vim/autoload/plug.vim" ]; then
+    echo "Istalling vim-plug..."
+    curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
+
+
+# -------------------------------
+# 3. Update/Install ~/.vimrc
+#--------------------------------
+
+VIMRC="$HOME/.vimrc"
+CUSTOM_VIMRC="custom_vimrc"
+
+if [ ! -f "$VIMRC" ]; then
+    echo "Creating initial ${VIMRC}..."
+    touch "$VIMRC"
+fi
+
+if ! grep -q "$CUSTOM_MARKER" "$VIMRC"; then
+    echo "Writing custom .vimrc lines to ${VIMRC}..."
+    echo "" >> "$VIMRC"
+    echo "$CUSTOM_MARKER" >> "$VIMRC"
+    echo "" >> "$VIMRC"
+    echo "$CUSTOM_VIMRC" >> "$VIMRC"
+fi
+
+
+# -------------------------------
+# 4. Perform PlugInstall
+#--------------------------------
+
+echo "Running PlugInstall..."
+vim +PlugInstall +qall
